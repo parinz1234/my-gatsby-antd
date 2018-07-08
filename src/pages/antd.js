a94c335376eafe6a13fd4b0cb78f8956b60b1f50
parinz1234 <parinya.ons@gmail.com>
2018-07-08 23:17:17 +0700

    add styled-components, antd

---
+++ src/pages/antd.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Button } from 'antd'
+
+const AntD = () => (
+  <div>
+    <h1>Hello World</h1>
+    <Button type="primary">Primary</Button>
+  </div>
+)
+
+export default AntD
---
+++ src/pages/styled-components.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Styled from 'styled-components'
+
+const Title = Styled.h1`
+  color: salmon;
+`
+
+const StyledComponents = () => (
+  <div>
+    <Title>Hello World</Title>
+  </div>
+)
+
+export default StyledComponents
